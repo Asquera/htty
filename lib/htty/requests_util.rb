d19620a53376f5c1bdc906654a2b0904e0435029
Florian Gilcher <flo@andersground.net>
2010-09-19 12:43:18 +0200

    First, hacky Rack::Test integration
    Uses Rack::Test instead of Net::HTTP to use HTTY
    as an App console.

--- lib/htty/requests_util.rb
+++ lib/htty/requests_util.rb
@@ -4,11 +4,74 @@ require 'net/http'
 require 'net/https'
 require 'uri'
 require File.expand_path("#{File.dirname __FILE__}/response")
+require 'rack/test'
 
 module HTTY; end
+module HTTY::Rack; end
+module HTTY::HTTP; end
+
+module HTTY::Rack::RequestsUtil
+  
+  module RackApp
+    def self.app
+      @app ||= build_app
+    end
+    
+    extend Rack::Test::Methods
+    
+    private
+      def self.build_app
+        config_file = File.read(find_config_file)
+        Rack::Builder.new { instance_eval(config_file) }.to_app
+      end
+      
+      def self.find_config_file
+        if Dir.glob("config.ru").length > 0
+          File.join(Dir.pwd,"config.ru")
+        elsif Dir.pwd != "/"
+          Dir.chdir("..") { find_config_file }
+        else
+          raise "Cannot find config.ru"
+        end
+      end
+  end
+  
+  class << self
+    %w(get post put head delete).each do |verb|
+      define_method(verb) do |request|
+        request(request) do
+          RackApp.send verb, '/'
+        end
+      end
+    end
+  end
+ 
+protected
+
+  def self.http_response_to_status(http_response)
+    [http_response.status, "bla"] #alpha...
+  end
+
+private
+
+  def self.request(request)
+    http_response = yield
+    headers = []
+    http_response.headers.each do |*h|
+      headers << h
+    end
+    request.send :response=,
+                 HTTY::Response.new(:status  => http_response_to_status(http_response),
+                                    :headers => headers,
+                                    :body    => http_response.body)
+    request
+  end
+
+end
+
 
 # Provides support for making HTTP(S) requests.
-module HTTY::RequestsUtil
+module HTTY::HTTP::RequestsUtil
 
   # Makes an HTTP DELETE request with the specified _request_.
   def self.delete(request)
