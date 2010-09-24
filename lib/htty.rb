fed97479a4f5921f9941adc99ad4aab344a069cb
Florian Gilcher <flo@andersground.net>
2010-09-24 21:04:10 +0200

    Removed the need to set HTTY::RequestUtil
    If you want a request that behaves differently,
    implement your own!

--- lib/htty.rb
+++ lib/htty.rb
@@ -5,14 +5,7 @@ module HTTY
 
   # The version of this release of _htty_.
   VERSION = File.read("#{File.dirname __FILE__}/../VERSION").chomp
-
-  def self.requests_util
-    @requests_util ||= HTTY::RequestsUtil
-  end
   
-  def self.requests_util=(util)
-    @requests_util = util
-  end
 end
 
 Dir.glob "#{File.dirname __FILE__}/htty/*.rb" do |f|
--- lib/htty/request.rb
+++ lib/htty/request.rb
@@ -462,7 +462,7 @@ private
   def request!(method)
     request = response ? dup_without_response : self
     request.instance_variable_set '@request_method', method
-    HTTY.requests_util.send method, request
+    HTTY::RequestsUtil.send method, request
   end
 
 end
